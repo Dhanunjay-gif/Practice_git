76685faf6ad081e0ca390a6a2953cf262ee18d0b
dhanunjay <boyadhanunjay01@gmail.com>
2025-01-21 09:38:46 +0530

    commited

---
+++ Practice/String_rev.js
@@ -0,0 +1,11 @@
+function reverseString (str){
+    return str.split('').reverse().join('')
+}
+
+console.log(reverseString("Dhanu"))
+
+const reverseStr = (str) =>{
+    return str.split('').reverse().join('')
+}
+
+console.log(reverseStr("DJ"))
